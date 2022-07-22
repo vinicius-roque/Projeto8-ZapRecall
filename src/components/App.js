6efefc3efe61e99bbbe2e666c5fa902255f9dfb5
Vinícius Roque <viniciusfr6@gmail.com>
2022-07-22 16:23:53 -0300

    Deck de Flashcards sendo exibido após clique em botão de iniciar

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,24 @@
+import React from 'react';
 import BoasVindas from "./BoasVindas";
+import TelaFlashcards from "./TelaFlashcards";
 import './style.css';
 import './reset.css';
 
 export default function App() {
+    const [tela, setTela] = React.useState(true);
+    
     return (
-        <BoasVindas />
+        <>
+            <div>
+                {tela ? (
+                    <div className="centralizar">
+                        <BoasVindas />
+                        <div className="botao" onClick={() => setTela(!tela)}>Iniciar Recall!</div>
+                    </div>
+                ) : (
+                    <TelaFlashcards />
+                )}
+            </div>
+        </>
     );
 }
